7d705ba107dcb8fba526f84ad40afbde4b3f0794
Petter Sæther Moen <pettersmoen@gmail.com>
2019-10-15 20:43:50 +0200

    Very hot navbar

--- src/Main.elm
+++ src/Main.elm
@@ -2,8 +2,9 @@ module Main exposing (..)
 
 import Browser
 import Css exposing (..)
+import Css.Transitions exposing (easeInOut, transition)
 import Html.Styled exposing (..)
-import Html.Styled.Attributes exposing (css)
+import Html.Styled.Attributes exposing (css, href)
 
 
 
@@ -36,6 +37,34 @@ update msg model =
 ---- VIEW ----
 
 
+navLink : String -> String -> Html msg
+navLink url t =
+    a
+        [ href url
+        , css
+            [ margin2 zero (rem 1)
+            , color (rgb 255 255 255)
+            , fontSize (rem 1.5)
+            , textDecoration none
+            , borderBottom3 (px 2) solid transparent
+            , transition
+                [ Css.Transitions.borderBottomColor3 150 0 easeInOut
+                ]
+            , hover
+                [ borderBottomColor (rgb 255 255 255)
+                ]
+            ]
+        ]
+        [ text t ]
+
+
+navLinks : Html msg
+navLinks =
+    div []
+        [ navLink "/" "Home", navLink "/test1" "Test 1", navLink "/test2" "Test 2" ]
+
+
+navBar : Html msg
 navBar =
     nav
         [ css
@@ -46,21 +75,30 @@ navBar =
             , width (pct 100)
             , height (rem 4)
             , padding2 zero (rem 1)
-            , backgroundColor (rgb 0 0 255)
+            , backgroundColor (rgb 58 112 207)
             , color (rgb 255 255 255)
             ]
         ]
-        [ span [ css [ fontSize (rem 2) ] ] [ text "N-Chess" ] ]
+        [ span
+            [ css
+                [ fontSize (rem 2) ]
+            ]
+            [ text "N-Chess" ]
+        , navLinks
+        ]
 
 
+headerSection : Html msg
 headerSection =
     navBar
 
 
+mainSection : Html msg
 mainSection =
     main_ [] []
 
 
+footerSection : Html msg
 footerSection =
     footer [] []
 
